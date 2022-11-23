f1c02f29334afc54e40f100087b62d78525696ff
shashwat-351 <103496887+shashwat-351@users.noreply.github.com>
2022-11-23 19:09:31 +0530

    created navbar and client and establish connection

--- django_part/app1/views.py
+++ django_part/app1/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def client(request):
+    return render(request,'client.html')
--- django_part/django_part/urls.py
+++ django_part/django_part/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import client
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',client)
+
 ]
